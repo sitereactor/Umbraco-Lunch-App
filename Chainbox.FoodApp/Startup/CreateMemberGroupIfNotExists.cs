aa5333205a5256c7b081c7516bd0fdbc665c09f0
Morten Christensen <mc@sitereactor.dk>
2014-06-16 16:04:39 +0200

    Adds startup check for the two Member Groups that are used on the site.

---
+++ Chainbox.FoodApp/Startup/CreateMemberGroupIfNotExists.cs
@@ -0,0 +1,27 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Models;
+
+namespace Chainbox.FoodApp.Startup
+{
+    public class CreateMemberGroupIfNotExists : ApplicationEventHandler
+    {
+        const string AdminGroupName = "Admin";
+        const string IntranetGroupName = "Intranet Users";
+
+        protected override void ApplicationStarted(UmbracoApplicationBase app, ApplicationContext ctx)
+        {
+            var memberGroupService = ctx.Services.MemberGroupService;
+            var adminGroup = memberGroupService.GetByName(AdminGroupName);
+            if (adminGroup == null)
+            {
+                memberGroupService.Save(new MemberGroup { Name = AdminGroupName });
+            }
+
+            var intranetGroup = memberGroupService.GetByName(IntranetGroupName);
+            if (intranetGroup == null)
+            {
+                memberGroupService.Save(new MemberGroup { Name = IntranetGroupName });
+            }
+        }
+    }
+}
